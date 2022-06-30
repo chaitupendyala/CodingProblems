f5f14a38351c37e2f377f4d45441b03f770356ae
chaitupendyala <prndyala@gmail.com>
2022-06-29 17:32:24 -0700

    Finished problem 55.

--- LeetCode/55_Jump_Game.py
+++ LeetCode/55_Jump_Game.py
@@ -13,8 +13,17 @@ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 Example 2:
 Input: nums = [3,2,1,0,4]
 Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+Explanation: You will always arrive at index 3 no matter what. 
+			 Its maximum jump length is 0, which makes it impossible to reach the last index.
 '''
 class Solution:
-	def canJump(self, nums: List[int]) -> bool:
-		pass
+	def canJump(self, nums: list[int]) -> bool:
+		m = 0
+		for i, n in enumerate(nums):
+			if i > m:
+				return False
+			m = max(i+n, m)
+		return True
+
+print("nums = [2,3,1,1,4]: ", Solution().canJump([2,3,1,1,4]))
+print("nums = [3,2,1,0,4]: ", Solution().canJump([3,2,1,0,4]))
