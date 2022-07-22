7f7ec2431d958fc4c88c017546ed790cb4d943ad
chaitupendyala <prndyala@gmail.com>
2022-07-21 20:13:55 -0700

    Finished problem 198.

---
+++ LeetCode/198_House_Robber.py
@@ -0,0 +1,50 @@
+'''
+198. House Robber
+Medium
+
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent 
+houses have security systems connected and it will automatically contact 
+the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+
+Constraints:
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+'''
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        if not nums:    return 0
+        length_of_nums = len(nums)
+        if length_of_nums < 3:
+            return max(nums)
+        OPT = [None for _ in range(length_of_nums)]
+        OPT[0] = nums[0]
+        OPT[1] = max(nums[0],nums[1])
+
+        for i in range(2,length_of_nums):
+            OPT[i] = max(OPT[i-1], nums[i]+OPT[i-2])
+        return OPT[-1]
+
+print("nums = [1,2,3,1]:", Solution().rob(nums = [1,2,3,1]))
+print("nums = [2,7,9,3,1]:", Solution().rob(nums = [2,7,9,3,1]))
+print("nums = [2,1,1,2]:", Solution().rob(nums = [2,1,1,2]))
+print("nums = [1,2]:", Solution().rob(nums = [1,2]))
+print("nums = [1,3,1]:", Solution().rob(nums = [1,3,1]))
+print("nums = [226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124]:", Solution().rob(nums=[226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124]))
+print("nums = [0]:", Solution().rob(nums=[0]))
