bbc35bae0a67d6f69e100d928cb1a851c82d8046
Phani Chaitanya Pendyala <prndyala@gmail.com>
2022-12-24 12:03:30 +0530

    Solution using Boyer-Moore Voting Algorithm.

--- LeetCode/169_Majority_Element.py
+++ LeetCode/169_Majority_Element.py
@@ -15,8 +15,24 @@ Output: 2
 
 Follow-up: Could you solve the problem in linear time and in O(1) space?
 '''
-from collections import Counter
+'''
+Boyer-Moore Voting Algorithm
+
+Essentially, what Boyer-Moore does is look for a suffix sufsufsuf of nums where 
+suf[0] is the majority element in that suffix. To do this, we maintain a count, 
+which is incremented whenever we see an instance of our current candidate for majority 
+element and decremented whenever we see anything else. Whenever count equals 0, 
+we effectively forget about everything in nums up to the current index and consider 
+the current number as the candidate for majority element. It is not immediately obvious 
+why we can get away with forgetting prefixes of nums - consider the following examples 
+(pipes are inserted to separate runs of nonzero count).
+'''
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        counts = Counter(nums)
-        return max(counts.keys(), key=counts.get)
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        return candidate
