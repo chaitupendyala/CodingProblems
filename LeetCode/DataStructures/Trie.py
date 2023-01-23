07f386d7ac35a1253c99997f179bc9c9ba29a5e6
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-01-22 23:27:37 -0800

    Added a new Data Structure implementation for Trie.

---
+++ LeetCode/DataStructures/Trie.py
@@ -0,0 +1,9 @@
+def makeTrie(words):
+    _end = "_end_"
+    root = dict()
+    for word in words:
+        current_dict = root
+        for letter in word:
+            current_dict = current_dict.setdefault(letter, {})
+        current_dict[_end] = _end
+    return root
