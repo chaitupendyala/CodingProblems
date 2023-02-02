ffb3f3b7fed34d48767ab67811161994d721c6b5
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-02-01 23:04:40 -0800

    Better approach for 290.

--- LeetCode/Programming/290_Word_Pattern.py
+++ LeetCode/Programming/290_Word_Pattern.py
@@ -1,18 +1,11 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         words = s.split(" ")
-        if len(words) != len(pattern):
+        if len(words) != len(pattern) and len(set(pattern)) != len(set(words)):
             return False
-        matching = {}
-        reverse_matching = {}
+        mapping = {}
         for i in range(len(pattern)):
-            print(pattern[i], words[i])
-            print(matching, reverse_matching)
-            if pattern[i] not in matching and words[i] not in reverse_matching:
-                matching[pattern[i]] = words[i]
-                reverse_matching[words[i]] = pattern[i]
-            elif pattern[i] in matching and words[i] in reverse_matching and matching[pattern[i]] == words[i] and reverse_matching[words[i]] == pattern[i]:
-                continue
-            else:
+            if pattern[i] in mapping and mapping[pattern[i]] != words[i]:
                 return False
+            mapping[pattern[i]] = words[i]
         return True
