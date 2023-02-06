f858195488bf3d9b979fc667a4795d76e9469828
Phani Chaitanya Pendyala <prndyala@gmail.com>
2023-02-05 19:55:23 -0800

    A more efficient code but less efficient interms of space.

--- LeetCode/Programming/1476_Subrectangle_Queries.py
+++ LeetCode/Programming/1476_Subrectangle_Queries.py
@@ -1,12 +1,14 @@
 class SubrectangleQueries:
 
     def __init__(self, rectangle: List[List[int]]):
-        self.rectangle = rectangle        
+        self.rectangle = rectangle
+        self.history = []   
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        for i in range(row1, row2+1):
-            for j in range(col1, col2+1):
-                self.rectangle[i][j] = newValue
+        self.history.append([row1, row2, col1, col2, newValue])
 
     def getValue(self, row: int, col: int) -> int:
+        for h in reversed(self.history):
+            if h[0] <= row <= h[1] and h[2] <= col <= h[3]:
+                return h[4]
         return self.rectangle[row][col]
